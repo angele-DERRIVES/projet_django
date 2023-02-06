57fb7fe0a289d0a6f52a947bcff65a917c58ace6
derrives <angele.derrives@etu.inuv-amu.fr>
2023-02-06 11:42:14 +0100

    vue attempt details

---
+++ Genealogie/views/attempt_detail_view.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect, get_object_or_404
+
+from Genealogie.models_directory.player import Player
+from Genealogie.models_directory.quiz import Quiz
+
+
+def AttemptDetail(request, quiz_id, attempt_id):
+    user = request.user
+    quiz = get_object_or_404(Quiz, id=quiz_id)
+    attempts = Player.objects.filter(quiz=quiz, player__user=user)
+
+    context = {
+        'quiz': quiz,
+        'attempts': attempts
+    }
+    return render(request, 'Quiz/attemptDetail.html', context)
