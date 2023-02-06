a141f43f903dcd2691f51c27959b16330dc5f4b1
derrives <angele.derrives@etu.inuv-amu.fr>
2023-02-06 11:20:38 +0100

    vue take_quiz terminée

--- Genealogie/views/take_quiz_view.py
+++ Genealogie/views/take_quiz_view.py
@@ -5,3 +5,7 @@ from Genealogie.models_directory.quiz import Quiz
 
 def TakeQuiz(request, quiz_id):
     quiz = get_object_or_404(Quiz, id=quiz_id)
+    context = {
+        'quiz': quiz,
+    }
+    return render(request, 'Quiz/takeQuiz.html', context)
