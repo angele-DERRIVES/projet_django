30dcb942ee4408df747dda9025ca5df5df7eacb2
derrives <angele.derrives@etu.inuv-amu.fr>
2023-02-09 08:59:12 +0100

    on peut rentrer des questions

--- Kouiz/models/question.py
+++ Kouiz/models/question.py
@@ -5,7 +5,7 @@ from Kouiz.models.answer import Answer
 
 
 class Question(models.Model):
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     question_text = models.CharField(max_length=900)
     answers = models.ManyToManyField(Answer)  # Multiple answers to one question
     points = models.PositiveIntegerField()
